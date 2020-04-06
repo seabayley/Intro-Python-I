81550c94a56d86e77cae1424c62aa33a8f8d9a41
Christopher Bailey <seabayley@outlook.com>
2020-04-06 15:54:05 -0500

    Added calendar and classes exercises

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,26 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+args = sys.argv
+month = int(args[1]) if args[1:] else False
+year = int(args[2]) if args[2:] else False
+
+if year and month:
+    print(calendar.month(year, month))
+elif month:
+    print(calendar.month(datetime.now().year, month))
+else:
+    print(calendar.month(datetime.now().year, datetime.now().month))
