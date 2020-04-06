f7fc24e02a54d72e8593e3b9b3a2ae2fc4d62ccc
Christopher Bailey <seabayley@outlook.com>
2020-04-06 05:48:08 -0500

    Added lists and tuples exercises

--- src/05_lists.py
+++ src/05_lists.py
@@ -7,23 +7,34 @@ y = [8, 9, 10]
 # For the following, DO NOT USE AN ASSIGNMENT (=).
 
 # Change x so that it is [1, 2, 3, 4]
-# YOUR CODE HERE
+x.append(4)
 print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
-# YOUR CODE HERE
+x.extend(y)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
-# YOUR CODE HERE
+x.remove(8)
 print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
-# YOUR CODE HERE
+for i in range(len(x)):
+    if x[i] == 9 and x[i+1] == 10:
+        x.insert(i+1, 99)
+        break
 print(x)
 
 # Print the length of list x
-# YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+
+# with a for loop
+stout = []
+for i in range(len(x)):
+    stout.append(x[i]*1000)
+print(stout)
+
+print(list(map(lambda x: x * 1000, x)))  # with map
+print([i*1000 for i in x])  # with comprehension
