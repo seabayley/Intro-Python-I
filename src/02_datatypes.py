daa881c6cd8f44fccbb781d4f4aa968ead092171
Christopher Bailey <seabayley@outlook.com>
2020-04-06 04:58:12 -0500

    Initial commit and added exercises 0-3

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -11,11 +11,25 @@ on a string and an integer.
 x = 5
 y = "7"
 
+
+def add_str_to_int(string, integer):
+    try:
+        return (int(string) + integer)
+    except ValueError:
+        print("That value cannot be coerced into an Integer.")
+
+
+def add_int_to_str(integer, string):
+    try:
+        return (str(integer) + string)
+    except TypeError:
+        print("That value cannot be coerced into a String.")
+
 # Write a print statement that combines x + y into the integer value 12
 
-# YOUR CODE HERE
 
+print(add_str_to_int(y, x))
 
 # Write a print statement that combines x + y into the string value 57
 
-# YOUR CODE HERE
+print(add_int_to_str(x, y))
